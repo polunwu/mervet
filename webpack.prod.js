026cf2501443c1a53a63f061e2bef75bf6c89b3b
polunwu <polunwu@gmail.com>
2020-08-05 23:44:50 +0800

    Add the stacking context structure and partial layouts

--- webpack.prod.js
+++ webpack.prod.js
@@ -70,7 +70,7 @@ module.exports = {
             },
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpe?g|gif)$/,
                 use: [
                     {
                         loader: 'url-loader',
