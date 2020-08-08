a33d8be93fb5e71374a70e758df0f21b5a4e9911
polunwu <polunwu@gmail.com>
2020-08-08 22:55:59 +0800

    Add menu & toggle feature

--- src/pages/home/js/_script.js
+++ src/pages/home/js/_script.js
@@ -1,1 +1,11 @@
-console.log('hi, this is mervet')
+window.addEventListener('load', () => {
+  // TOGGLE MENU
+  const menu = document.querySelector('.js-menu')
+  const toggleNav = document.querySelector('.js-toggle-nav')
+
+  toggleNav.addEventListener('click', () => {
+    menu.classList.toggle('is-open')
+    toggleNav.classList.toggle('is-open')
+  })
+  // END OF TOGGLE MENU
+})
