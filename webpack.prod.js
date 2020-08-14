0b618f70ce16fe4741ef47ccb8ca2d6e78047f8e
polunwu <polunwu@gmail.com>
2020-08-15 00:58:07 +0800

    Add og settings & favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -142,7 +142,7 @@ module.exports = {
             // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
             background: '#fff',
             // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-            title: 'my-project',
+            title: 'Mervét｜梅爾瓦特',
 
             // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
             icons: {
